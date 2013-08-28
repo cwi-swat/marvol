6040b5e88b464b8fe48770492a1a60dcab45403e
Atze van der Ploeg <A.J.van.der.Ploeg@cwi.nl>
2013-08-28 11:49:18 +0200

    Move to initial pose first

--- src/lang/marvol/backend/MoveRunnable.java
+++ src/lang/marvol/backend/MoveRunnable.java
@@ -4,24 +4,24 @@ import java.util.ArrayList;
 
 import lang.marvol.backend.pos.NewPose;
 
+public class MoveRunnable implements Runnable {
 
-public class MoveRunnable implements Runnable{
+	public static final float Duration = 1.75f;
+	final ArrayList<NewPose> poses;
 
+	public MoveRunnable(ArrayList<NewPose> poses) {
+		this.poses = poses;
+	}
 
-		final ArrayList<NewPose> poses;
-		
-		public MoveRunnable( ArrayList<NewPose> poses){
-			this.poses = poses;
+	@Override
+	public void run() {
+		NewPose.Init.goToMe(DanceInterpreter.mp, Duration);
+		for (NewPose i : poses) {
+			i.goToMe(DanceInterpreter.mp, Duration);
+			if (DanceInterpreter.shouldStop)
+				return;
 		}
-		
-		@Override
-		public void run() {
-			for(NewPose i : poses){
-				i.goToMe(DanceInterpreter.mp, 1.75f);
-				if(DanceInterpreter.shouldStop) return;
-			}
-			DanceInterpreter.curThread = null;
-		}
-		
+		DanceInterpreter.curThread = null;
+	}
+
 }
-
